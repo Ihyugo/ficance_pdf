c1800a3ad2de1bce6e320fc9fed751bed720cdd6
hyugo <yugo1997.10.14@toki.waseda.jp>
2020-06-21 17:03:13 +0900

    1ページの部分撮り直し

--- make_screenshot.py
+++ make_screenshot.py
@@ -4,14 +4,22 @@ import time
 import os
 
 #mywasedaのログインIDとパス
-login_id = "6r1300228"
-password = "carryriceIY"
+login_id = ""
+password = ""
+kind = None
+
+if login_id == "":
+    login_id = input("MyWasedaのログインID:")
+    password = input("MyWasedaのログインPassword:")
 
 def main():
     login_moodle()
     open_finance_pdf()
     move_to_pdf_data()
     make_png_file()
+    make_png_file_by_id()
+
+
 
 def login_moodle():
     moodle_login = driver.find_element_by_class_name("potentialidp")
@@ -26,7 +34,10 @@ def login_moodle():
 
 def open_finance_pdf():
     options = driver.find_elements_by_class_name("activityinstance")
-    options[-2].click()
+    if kind is not None:
+        options[3*kind-1].click()
+    else:
+        options[-2].click()
     tab_list = driver.window_handles
     driver.switch_to.window(tab_list[1])
     driver.maximize_window()
@@ -43,13 +54,22 @@ def move_to_pdf_data():
     driver.get(src)
     time.sleep(3)
 
+def make_png_file_by_id():
+    i = 2
+    id_tag = "page-container" + str(i)
+    png = driver.find_element_by_id(id_tag).screenshot_as_png
+    string = "img_" + str(i) + ".png"
+    with open("./png/"+string, "wb") as f:
+        f.write(png)
+
 def make_png_file():
     classes = driver.find_elements_by_class_name("page-container")
+
     i = 0
     for img in classes:
         i = i + 1
         string = "img_" + str(i) + ".png"
-        png = img.screenshot_as_png
+        png = img.find_element_by_class_name("canvas").screenshot_as_png
         with open("./png/"+string, "wb") as f:
             f.write(png)
 
