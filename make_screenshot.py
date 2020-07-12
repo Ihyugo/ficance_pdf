2e6cf2978b519c9f4a52d4da145f718670811aae
hyugo <yugo1997.10.14@toki.waseda.jp>
2020-07-12 11:47:00 +0900

    動画自動再生機能の追加

--- make_screenshot.py
+++ make_screenshot.py
@@ -1,11 +1,14 @@
 from selenium import webdriver #Selenium Webdriverをインポートして
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import base64
 import time
 import os
 
 #mywasedaのログインIDとパス
-login_id = ""
-password = ""
+login_id = "6r1300228"
+password = "carryriceIY"
 kind = None
 
 if login_id == "":
@@ -18,7 +21,8 @@ def main():
     move_to_pdf_data()
     make_png_file()
     make_png_file_by_id()
-
+    move_to_pdf_data()
+    play_movie()
 
 
 def login_moodle():
@@ -38,8 +42,9 @@ def open_finance_pdf():
         options[3*kind-1].click()
     else:
         options[-2].click()
+        options[-3].click()
     tab_list = driver.window_handles
-    driver.switch_to.window(tab_list[1])
+    driver.switch_to.window(tab_list[-1])
     driver.maximize_window()
     time.sleep(3)
 
@@ -61,6 +66,9 @@ def make_png_file_by_id():
     string = "img_" + str(i) + ".png"
     with open("./png/"+string, "wb") as f:
         f.write(png)
+    driver.close()
+    tab_list = driver.window_handles
+    driver.switch_to.window(tab_list[-1])
 
 def make_png_file():
     classes = driver.find_elements_by_class_name("page-container")
@@ -72,6 +80,23 @@ def make_png_file():
         png = img.find_element_by_class_name("canvas").screenshot_as_png
         with open("./png/"+string, "wb") as f:
             f.write(png)
+
+
+def play_movie():
+    screens = driver.find_element_by_class_name("vc-pctrl-play-pause-btn")
+    screens.click()
+    screens.click()
+    time.sleep(10)
+    try:
+        WebDriverWait(driver, 3).until(EC.alert_is_present(),
+                                       'Timed out waiting for PA creation ' +
+                                       'confirmation popup to appear.')
+
+        alert = driver.switch_to.alert
+        alert.accept()
+        print("alert accepted")
+    except TimeoutException:
+        print("no alert")
 
 if __name__ == '__main__':
     try:
